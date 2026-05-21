c189c4b07cd8c93285c0a291a68f513ed82fe161
Aditya Patil <adityarvindapatil@gmail.com>
2026-05-21 11:25:13 +0530

    adjustment in frontend  commit

--- server/index.js
+++ server/index.js
@@ -1,34 +1,25 @@
-require("node:dns/promises").setServers(["1.1.1.1", "8.8.8.8"]); 
-const express = require('express');
-const cors = require('cors');
-require('dotenv').config();
+const express = require("express");
+const cors = require("cors");
+require("dotenv").config();
 
-const projectRoutes = require('./routes/projectRoutes');
-const contactRoutes = require('./routes/contactRoutes');
-const connectDB = require('./config/db');
+const projectRoutes = require("./routes/projectRoutes");
+const contactRoutes = require("./routes/contactRoutes");
+const connectDB = require("./config/db");
 
 const app = express();
-const PORT = process.env.PORT || 5000;
 
-// Middleware
 app.use(cors());
 app.use(express.json());
 
-app.use(express.static(
-  path.join(__dirname, "../build")
-));
+// API routes
+app.use("/api/projects", projectRoutes);
+app.use("/api/contact", contactRoutes);
 
-app.get("*", (req, res) => {
-  res.sendFile(
-    path.join(__dirname, "../dist/index.html")
-  );
+// test route
+app.get("/", (req, res) => {
+  res.json({ message: "API Running 🚀" });
 });
-// Routes
-app.use('/api/projects', projectRoutes);
-app.use('/api/contact', contactRoutes);
 
+connectDB();
 
-app.listen(PORT, () => {
-  connectDB();
-  console.log(`Server running on port ${PORT}`);
-});
+module.exports = app;
