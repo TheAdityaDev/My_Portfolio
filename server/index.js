8c694b24fa56bbafc9fc9ff47cc7e1e0972c51e1
Aditya Patil <adityarvindapatil@gmail.com>
2026-05-21 11:22:05 +0530

    adjustment  commit

--- server/index.js
+++ server/index.js
@@ -24,7 +24,7 @@ app.use(express.static(
 
 app.get("*", (req, res) => {
   res.sendFile(
-    path.join(__dirname, "../client/dist/index.html")
+    path.join(__dirname, "../dist/index.html")
   );
 });
 
